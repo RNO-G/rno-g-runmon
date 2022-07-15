d5d0278bd37317a4182cf550fac8fc8a2a38d4e6
Matthew Marsee <mattjmarsee@gmail.com>
2022-07-15 20:14:04 +0000

    Fills tables for each (ingress,rootified,archived,outbox) subdir of /data with most recently updated run from each station and updates every 30 seconds.  Includes the time updated, time accessed, and size of all files in the run directory

--- run.py
+++ run.py
@@ -4,9 +4,10 @@
 import time 
 #for db connection
 import psycopg2 
-
+import pathlib,os,datetime,glob
 #for signal handler
 import signal 
+import numpy as np
 
 db_credentials="dbname=rno_g_runmon user=runmon" 
 
@@ -26,7 +27,11 @@ def get_dir_size(path):
 
 def check_dir(db_conn, table_name, check_dir): 
     # this function will find the stations in check_dir, loop over them, 
-    stations = ['11','12','13','21','22','23'] 
+    stnp=glob.glob(check_dir+'/station*')
+    stations=[]
+    for s in stnp: stations.append(str(s)[-2:])
+    stations.sort(key=int)
+    stations=[str(x) for x in stations]
     #these indices will correspond to the respective stations in future arrays
     latrunpath=[] #path to each latest run
     latruns=[] #num of latest run for each station
@@ -34,42 +39,42 @@ def check_dir(db_conn, table_name, check_dir):
     fsize=[] #size of run file
 
     #this grabs the path of the last modified run for each station & its timestamp
+    print(table_name)
     for stn in stations:
-            #print(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime))
-            latrunpath.append(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime))
-            #print(datetime.datetime.fromtimestamp(os.path.getmtime(max(pathlib.Path('/data/ingress-handcarry/station'+stn+'/').glob('run*/'), key=os.path.getmtime))))
-            tmstp.append(datetime.datetime.fromtimestamp(os.path.getmtime(max(pathlib.Path('/data/ingress-handcarry/station'+stn+'/').glob('run*/'), key=os.path.getmtime))))
+            print(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime))
+            latrunpath.append(str(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime)))
+            print(datetime.datetime.fromtimestamp(os.path.getmtime(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime))))
+            tmstp.append(datetime.datetime.fromtimestamp(os.path.getmtime(max(pathlib.Path(check_dir+'/station'+stn+'/').glob('run*/'), key=os.path.getmtime))))
 
     #this grabs the name of the run directory and the combined size of all files within  
     for pt in latrunpath:
         latruns.append(pt.split(os.sep)[-1]) #name of each run
-        fsize.append(str(np.round(float(get_dir_size(pt)/1024),2))+' KB') #total file size
+        fsize.append((np.round(float(get_dir_size(pt)/1024),2))) #total file size kb
 
 
     #pre-existing titles in the database table: [station_number, run_num,time_mod, time_accessed,size_kb]
 
     ## SQL SECTION STARTS HERE ##################
+    cursor = db_conn.cursor()
     try:
-        cursor = db_conn.cursor()
+    #    cursor = db_conn.cursor()
 
         #one entry for each station
         for i in range(len(stations)):
-            q="""INSERT INTO """+table_name+""" (%s,%s,%s,%s,%f)"""
-            qval=('station'+stations[i], str(latrun[i]),str(tmstp[i]),str(datetime.datetime.now()),fsize[i])
+            q="INSERT INTO "+table_name+" (station,run,time_mod,time_now,size_kb) VALUES (%s,%s,%s,%s,%s) RETURNING id;"
+            qval=('station'+stations[i], str(latruns[i]),str(tmstp[i]),str(datetime.datetime.now()),fsize[i])
 
-            cursor.execute(q,qval) #inset entry into table
+            cursor.execute(q,qval) #insert entry into table
 
-        cursor.execute("""grant SELECT on TABLE """+table_name+""" to grafana""")#send table to grafana
-
-        db_conn.commit()
+            db_conn.commit()
 
     except (Exception, psycopg2.Error) as error:
-    print("Failed to insert data", error)
+        print("Failed to insert data\nError: ", error)
 
     finally:
         if db_conn:
             cursor.close()
-            db_conn.close()
+#            db_conn.close()
             print("PostgreSQL connection is closed")
     
     ##SQL SECTION ENDS HERE #######################
@@ -88,15 +93,14 @@ if __name__=='__main__':
     #set up signal handlers 
     signal.signal(signal.SIGINT, handler)
     signal.signal(signal.SIGTERM, handler)
-    global time_to_stop
+    time_to_stop
 
     #connect to database, and loop until we're told not to 
     with psycopg2.connect(db_credentials) as conn: 
         while not time_to_stop: 
-            time.sleep(10) 
             check_dir(conn, "ingress","/data/ingress")
             check_dir(conn, "rootified","/data/rootified")
-            check_dir(conn, "outbox","/data/outbox")
             check_dir(conn, "archived","/data/archived")
-
+            check_dir(conn, "outbox","/data/outbox")
+            time.sleep(30)
 
